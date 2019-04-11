34fa6884df0c3478615e0030357d14f683441e26
Spencer <swhite215@live.com>
2019-04-10 22:06:33 -0400

    first example contract in solidity

---
+++ Ethereum/firstContract.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.17; //Specifies version of Solidity
+
+contract Inbox { //Definition of contract class
+    string public message; //Variable declaration and their types
+    
+    //Contract Member - Same Name, Constructor, Called Once
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    
+    //Contract Member
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+    
+    //getMessage = Name, public view = Type, returns (string) = Return Types
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
+
