af90d56ce211c7ffa2070b85b70fb6e5b906f468
Spencer <swhite215@live.com>
2019-04-12 11:41:54 -0400

    addresed uint types and missing values

--- Ethereum/campaignContract/contracts/campaign.sol
+++ Ethereum/campaignContract/contracts/campaign.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.17; //Specifies version of Solidity
 
 contract Campaign {
     address public manager;
-    unint public minimumContribution;
+    uint public minimumContribution;
     address[] public approvers;
     
     //On initialization, save the contract creator as the manager
-    function Campaign(unint minimum) public {
+    function Campaign(uint minimum) public {
         manager = msg.sender;
-        minimumContribution = 
+        minimumContribution = minimum;
     }
     
     //Payable, expecting some money possible w/ require of minimum
     function contribute() public payable {
-        require(msg.value >= minimumContribution)
+        require(msg.value >= minimumContribution);
         approvers.push(msg.sender);
     }
 }
