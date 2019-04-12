2afaca2112521566a5cfe1cf94d3255aef16bb38
Spencer <swhite215@live.com>
2019-04-12 12:37:48 -0400

    added createRequest method using memory

--- Ethereum/campaignContract/contracts/campaign.sol
+++ Ethereum/campaignContract/contracts/campaign.sol
@@ -1,11 +1,6 @@
 pragma solidity ^0.4.17; //Specifies version of Solidity
 
 contract Campaign {
-    //Variables
-    address public manager;
-    uint public minimumContribution;
-    address[] public approvers;
-    
     //Struct - Definitions
     struct Request {
         string description;
@@ -14,15 +9,39 @@ contract Campaign {
         bool complete;
     }
     
-    //On initialization, save the contract creator as the manager
+    //Variables
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+    Request[] public requests;
+    
+    //Modifiers
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+    
+    //constructor, save the contract creator as the manager
     function Campaign(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
     
-    //Payable, expecting some money possible w/ require of minimum
+    //contribute method, payable, require contribution, add to approvers
     function contribute() public payable {
         require(msg.value >= minimumContribution);
         approvers.push(msg.sender);
+    }
+    
+    //createRequest method, creates request struct and adds it to request array
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false
+        });
+        
+        requests.push(newRequest);
     }
 }
