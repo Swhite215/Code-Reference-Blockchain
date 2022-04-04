004775f96e46de784b0a0519349496fd83b9cf99
Spencer White <swhite215@live.com>
2022-04-04 11:45:18 -0400

    hardhat reference material

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -1,4 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("./scripts/sub-tasks.js");
+
+// Example Task
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs, hre, runSuper) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
---
+++ Hardhat/scripts/sub-tasks.js
@@ -0,0 +1,11 @@
+task("hello-world", "Prints a hello world message").setAction(
+    async (taskArgs, hre) => {
+      await hre.run("print", { message: "Hello, World!" });
+    }
+  );
+  
+subtask("print", "Prints a message")
+    .addParam("message", "The message to print")
+    .setAction(async (taskArgs) => {
+        console.log(taskArgs.message);
+    });
